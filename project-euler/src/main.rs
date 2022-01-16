f97f240953ab31e19e03d344458dd4850f71bda3
atomasz <aaron.tomasz@gmail.com>
2022-01-16 23:56:45 +0000

    rusting

--- project-euler/src/main.rs
+++ project-euler/src/main.rs
@@ -10,17 +10,15 @@ fn main() {
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut numberIndex = HashMap::new();
-        let mut result:Vec<i32> = Vec::new();
-        for i in 0..nums.len(){
-            let num = nums[i];
-            let complement = target - num;
-            if numberIndex.contains_key(&complement)
-            {
-                result.push(*numberIndex.get(&complement).unwrap());
-                result.push(i as i32);
-                return result;
-            }
-            numberIndex.insert(num,i as i32);
+    let mut result:Vec<i32> = Vec::new();
+    for (i, num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if numberIndex.contains_key(&complement) {
+            result.push(*numberIndex.get(&complement).unwrap());
+            result.push(i as i32);
+            return result;
         }
-	    result
+        numberIndex.insert(num,i as i32);
+    }
+    result
 }
