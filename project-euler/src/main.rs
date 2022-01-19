cbe2ef3a7975bed8c212c28bb9b2d4d2f175bcc2
atomasz <aaron.tomasz@gmail.com>
2022-01-19 22:34:03 +0000

    borrow checker

--- project-euler/src/main.rs
+++ project-euler/src/main.rs
@@ -15,10 +15,19 @@ fn main() {
 fn test_sum_two() {
     let mut rng = rand::thread_rng();
     let length_one: u8 = rng.gen_range(5..20);
-    let rng_vector = create_random_vec_of_int(length_one, rng);
+    let rng_vector = create_random_vec_of_int(length_one, &mut rng);
     println!("{:?}", rng_vector);
+    let sorted_rng_vector = create_random_vec_of_sorted_int(length_one, &mut rng);
+    println!("{:?}", sorted_rng_vector);
 }
 
-fn create_random_vec_of_int(length: u8, mut rng: ThreadRng) -> Vec<i32> {
+fn create_random_vec_of_int(length: u8, rng: &mut ThreadRng) -> Vec<i32> {
     (0..length).map(|_| rng.gen_range(0..1000)).collect()
 }
+
+fn create_random_vec_of_sorted_int(length: u8, rng: &mut ThreadRng) -> Vec<i32> {
+    let mut rng_vector = create_random_vec_of_int(length, rng);
+    rng_vector.sort();
+    rng_vector
+}
+
